d67611cecdab1547896ec2e1d7a908e54818acae
Martin R <martin.ristvedt@gmail.com>
2026-09-21 23:49:03 +0200

    kommentar

--- src/components/SimpleCounter.ts
+++ src/components/SimpleCounter.ts
@@ -2,7 +2,7 @@ export class SimpleCounter extends HTMLElement {
     private count = 0;
     private step = 1;
 
-    static observedAttributes = ["heading"];
+    static observedAttributes = ["start-value", "step"];
 
     constructor() {
         super();
@@ -13,8 +13,14 @@ export class SimpleCounter extends HTMLElement {
         this.render();
     }
 
-    attributeChangedCallback() {
-
+    attributeChangedCallback(
+        name: string,
+        oldValue: string | null,
+        newValue: string | null
+    ) {
+        console.log(name, oldValue, newValue);
+        // blir én endret, kalles dette attributechangedcallback to ganger, én til å beskrive gamle verdien, andre til å beskrive nye verdien. Endrer jeg verdiene til step og count på samme tid gir den meg kun utslag til de nye endringene, og gir meg ikke melding om de gamle propertisene sine verdier.
+        // selv om jeg endrer så endrer ikke objektene seg i det hele tatt. Jeg mottar melding om at det var *forsøk* på å endre.
     }
     private render(): void {
         this.innerHTML = `<button>Antall: ${this.count}</button>`;
